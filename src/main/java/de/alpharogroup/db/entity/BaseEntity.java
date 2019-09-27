1595dd9ca79f0752c7a2e2bf73b0f2a9b0ae1639
astrapi69 <asterios.raptis@gmx.net>
2019-09-27 14:48:22 +0200

    deleted test base entity BaseSequenceEntity
    changed strategy to SEQUENCE for the BaseEntity

--- src/main/java/de/alpharogroup/db/entity/BaseEntity.java
+++ src/main/java/de/alpharogroup/db/entity/BaseEntity.java
@@ -58,34 +58,45 @@ import lombok.experimental.FieldDefaults;
 public abstract class BaseEntity<PK extends Serializable> implements Serializable, Identifiable<PK>
 {
 
-	/** The constant for the foreign key prefix
-	 * @deprecated use instead the same name constant from <code><DatabasePrefix/code>
-	 * <br>
-	 * Note: will be removed in the next minor release version
+	/**
+	 * The Constant for the generic sequence name. Note this must be given as name in the annotation
+	 * SequenceGenerator if you want a sequence for the specific entity.
+	 */
+	public static final String SEQUENCE_GENERIC_GENERATOR_NAME = "hibernate_sequence";
+
+	/**
+	 * The constant for the foreign key prefix
+	 *
+	 * @deprecated use instead the same name constant from <code><DatabasePrefix/code> <br>
+	 *             Note: will be removed in the next minor release version
 	 **/
 	@Deprecated
 	public static final String FOREIGN_KEY_PREFIX = DatabasePrefix.FOREIGN_KEY_NAME.getPrefix();
 
-	/** The constant for the index prefix
-	 * @deprecated use instead the same name constant from <code><DatabasePrefix/code>
-	 * <br>
-	 * Note: will be removed in the next minor release version
+	/**
+	 * The constant for the index prefix
+	 *
+	 * @deprecated use instead the same name constant from <code><DatabasePrefix/code> <br>
+	 *             Note: will be removed in the next minor release version
 	 **/
 	@Deprecated
 	public static final String INDEX_PREFIX = DatabasePrefix.INDEX_NAME.getPrefix();
 
-	/** The constant for the sequence generator prefix
-	 * @deprecated use instead the same name constant from <code><DatabasePrefix/code>
-	 * <br>
-	 * Note: will be removed in the next minor release version
+	/**
+	 * The constant for the sequence generator prefix
+	 *
+	 * @deprecated use instead the same name constant from <code><DatabasePrefix/code> <br>
+	 *             Note: will be removed in the next minor release version
 	 **/
 	@Deprecated
-	public static final String SEQUENCE_GENERATOR_PREFIX = DatabasePrefix.SEQUENCE_GENERATOR_NAME.getPrefix();
+	public static final String SEQUENCE_GENERATOR_PREFIX = DatabasePrefix.SEQUENCE_GENERATOR_NAME
+		.getPrefix();
 
-	/** The constant for the sequence prefix
-	 * @deprecated use instead the same name constant from <code><DatabasePrefix/code>
-	 * <br>
-	 * Note: will be removed in the next minor release version
+	/**
+	 * The constant for the sequence prefix
+	 *
+	 * @deprecated use instead the same name constant from <code><DatabasePrefix/code> <br>
+	 *             Note: will be removed in the next minor release version
 	 **/
 	@Deprecated
 	public static final String SEQUENCE_PREFIX = DatabasePrefix.SEQUENCE_NAME.getPrefix();
@@ -93,26 +104,29 @@ public abstract class BaseEntity<PK extends Serializable> implements Serializabl
 	/** The serialVersionUID. */
 	private static final long serialVersionUID = 1L;
 
-	/** The constant for the underscore
-	 * @deprecated use instead the same name constant from <code><DatabasePrefix/code>
-	 * <br>
-	 * Note: will be removed in the next minor release version
+	/**
+	 * The constant for the underscore
+	 *
+	 * @deprecated use instead the same name constant from <code><DatabasePrefix/code> <br>
+	 *             Note: will be removed in the next minor release version
 	 **/
 	@Deprecated
 	public static final String UNDERSCORE = DatabasePrefix.UNDERSCORE.getPrefix();
 
-	/** The constant for the unique constraint prefix
-	 * @deprecated use instead the same name constant from <code><DatabasePrefix/code>
-	 * <br>
-	 * Note: will be removed in the next minor release version
+	/**
+	 * The constant for the unique constraint prefix
+	 *
+	 * @deprecated use instead the same name constant from <code><DatabasePrefix/code> <br>
+	 *             Note: will be removed in the next minor release version
 	 **/
 	@Deprecated
-	public static final String UNIQUE_CONSTRAINT_PREFIX = DatabasePrefix.UNIQUE_CONSTRAINT_NAME.getPrefix();
+	public static final String UNIQUE_CONSTRAINT_PREFIX = DatabasePrefix.UNIQUE_CONSTRAINT_NAME
+		.getPrefix();
 
 	/** The technical primary key. */
 	@Id
 	@EqualsAndHashCode.Include
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = BaseEntity.SEQUENCE_GENERIC_GENERATOR_NAME)
 	@Column(name = "id", nullable = false)
 	PK id;
 
