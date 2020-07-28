5e7356bc1a616579956a2666e076adc2a5fb0056
astrapi69 <asterios.raptis@gmx.net>
2020-07-28 17:35:32 +0200

    new entity class ByCreation that refects the old Creation
    - new entity class ByDeletion that refects the old Deletion
    - new entity class ByLastModification that refects the old LastModification
    - removed the field createdBy from the class Creation
    - removed the field deletedBy from the class Deletion
    - removed the field lastModifiedBy from the class LastModification

--- src/main/java/de/alpharogroup/db/entity/creatable/ByCreation.java
+++ src/main/java/de/alpharogroup/db/entity/creatable/ByCreation.java
@@ -24,39 +24,36 @@
  */
 package de.alpharogroup.db.entity.creatable;
 
-import java.io.Serializable;
+import de.alpharogroup.db.entity.base.SequenceBaseEntity;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
-
-import de.alpharogroup.db.entity.base.SequenceBaseEntity;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-import lombok.experimental.SuperBuilder;
+import java.io.Serializable;
 
 /**
- * The entity class {@link Creation} is keeping the information for the creation of an entity. This
+ * The entity class {@link ByCreation} is keeping the information for the creation of an entity. This
  * entity can be extended or attached to another entity for keep information when it was created.
  *
  * @param <PK>
  *            the generic type of the id
  * @param <T>
  *            the generic type of time measurement
+ * @param <U>
+ *            the generic type of the user or account
  */
 @Entity
-@Table(name = "creation")
+@Table(name = "creation_by")
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
-public class Creation<PK extends Serializable, T> extends SequenceBaseEntity<PK>
+public class ByCreation<PK extends Serializable, T, U> extends SequenceBaseEntity<PK>
 	implements
-		IdentifiableCreatable<PK, T>
+		IdentifiableByCreatable<PK, T, U>
 {
 
 	/** The Constant serialVersionUID. */
@@ -64,5 +61,8 @@ public class Creation<PK extends Serializable, T> extends SequenceBaseEntity<PK>
 
 	/** The date and time when the entity that owns this entity was created. */
 	private T created;
+
+	/** The user or account that created the entity that owns this entity. */
+	private U createdBy;
 
 }
--- src/main/java/de/alpharogroup/db/entity/modifiable/ByLastModification.java
+++ src/main/java/de/alpharogroup/db/entity/modifiable/ByLastModification.java
@@ -22,47 +22,49 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package de.alpharogroup.db.entity.creatable;
+package de.alpharogroup.db.entity.modifiable;
 
-import java.io.Serializable;
+import de.alpharogroup.db.entity.base.SequenceBaseEntity;
+import de.alpharogroup.db.entity.versionable.VersionableEntity;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
-
-import de.alpharogroup.db.entity.base.SequenceBaseEntity;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-import lombok.experimental.SuperBuilder;
+import java.io.Serializable;
 
 /**
- * The entity class {@link Creation} is keeping the information for the creation of an entity. This
- * entity can be extended or attached to another entity for keep information when it was created.
+ * The entity class {@link ByLastModification} is keeping information of the last modification of an
+ * entity. This entity can be extended or attached to another entity for keep information when it
+ * was last modified.
  *
  * @param <PK>
  *            the generic type of the id
  * @param <T>
  *            the generic type of time measurement
+ * @param <U>
+ *            the generic type of the user or account
  */
 @Entity
-@Table(name = "creation")
+@Table(name = "modified_by")
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
-public class Creation<PK extends Serializable, T> extends SequenceBaseEntity<PK>
+public class ByLastModification<PK extends Serializable, T, U> extends SequenceBaseEntity<PK>
 	implements
-		IdentifiableCreatable<PK, T>
+		IdentifiableByLastModified<PK, T, U>
 {
 
 	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 1L;
 
-	/** The date and time when the entity that owns this entity was created. */
-	private T created;
+	/** The date and time when the entity that owns this entity was modified. */
+	private T lastModified;
+
+	/** The user or account that modified the entity that owns this entity. */
+	private U lastModifiedBy;
 
 }
